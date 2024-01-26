e2d4cfc02c146a18d81e3705cf5c8f99034013cd
Muhammed Taha Çınar <tahacinar@outlook.com.tr>
2024-01-26 16:38:11 +0300

    v1.0.

--- app/src/main/java/com/tahacinar/degiskenveritipleri/15-DegiskenOlusturmaCesitleri.kt
+++ app/src/main/java/com/tahacinar/degiskenveritipleri/15-DegiskenOlusturmaCesitleri.kt
@@ -2,5 +2,35 @@ package com.tahacinar.degiskenveritipleri
 
 fun main (){
 
+    var sayi = 10
+
+    //var etiketi ile kullandığımız değerleri sonradan değiştirebiliriz.
+
+    var fiyat = 12.99
+
+    println(fiyat)
+
+    fiyat = 10.99
+
+    println(fiyat) // Burada yeni değerimiz ekrana yazdıralacaktır.
+
+
+    // değişken içerisinde toplama işlemi yapabiliriz.
+    var s1 = 20
+    var s2 = 30
+
+    var toplam = s1 + s2
+    println(toplam)
+
+
+    // Type Safety
+    // Sonuc değişkenine bir tam sayı atadığımız için bu değeri sadece tam sayı ile değşitirebiliriz.
+    var sonuc = 100
+
+
+
+
+
+
 
 }
